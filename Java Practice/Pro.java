50c67848918f284f1dd8a1727c47e395273276f7
Ayush-Sharma3011 <ayush.sharma235@lpu.in>
2025-01-17 17:20:10 +0530

    java practice

--- Java Practice/Pro.java
+++ Java Practice/Pro.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
 class Pro{
-public static void main(String[]args){
-System.out.println("Hello World");
+// System.out.println("Hello World");
+// Scanner sc = new Scanner(System.in);
+// System.out.println("Enter the first number");
+
+// int num1 = sc.nextInt();
+
+// System.out.println("Enter the second number");
+
+// int num2 = sc.nextInt();
+
+// int sum = num1 + num2;
+
+// System.out.println("Sum of " + num1 + " and " + num2 + " is " + sum);
+public static void main(String[] args) {
+    //type your code here
+    Scanner sc= new Scanner(System.in);
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    int c=sc.nextInt();
+    int d=sc.nextInt();
+    int result= ((a+b-c*d / a) % b);
+    System.out.printf("(a + b - c * d / a) %%b = %d", result);
+
+    sc.close();
 }
 }
